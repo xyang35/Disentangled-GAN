aac3a54061b53d48e601806b098177cf42466760
xyang35 <yangxitongbob@gmail.com>
2017-08-30 14:28:12 -0700

    sigma filter

--- tools/sigma_filter.py
+++ tools/sigma_filter.py
@@ -1,5 +1,4 @@
 import numpy as np
-from multiprocessing import Pool
 
 """
 Implementation of Sigma filter (Lee's filter)
@@ -22,40 +21,37 @@ def boxfilter(imSrc, r):
     # difference over Y axis
     imDst[:r+1, :] = imCum[r:2*r+1, :]
     imDst[r+1:hei-r, :] = imCum[2*r+1:hei, :] - imCum[:hei-2*r-1, :]
-    imDst[hei-r:hei, :] = imCum[hei-1, :] - imCum[hei-2*r-1:hei-r-1, :]
+    imDst[hei-r:hei, :] = imCum[hei-1, :].reshape(1,-1) - imCum[hei-2*r-1:hei-r-1, :]
 
     # cumulative sum over X axis
     imCum = np.cumsum(imDst, axis=1)
     # difference over Y axis
     imDst[:, :r+1] = imCum[:, r:2*r+1]
     imDst[:, r+1:wid-r] = imCum[:, 2*r+1:wid] - imCum[:, :wid-2*r-1]
-    imDst[:, wid-r:wid] = imCum[:, wid] - imCum[:, wid-2*r-1:wid-r-1]
+    imDst[:, wid-r:wid] = imCum[:, wid-1].reshape(-1,1) - imCum[:, wid-2*r-1:wid-r-1]
 
     return imDst
 
 
 class SigmaFilter(object):
-    def __init__(self, radius=2, sigma=2., min_frac=0.2, pool=1):
+    def __init__(self, radius=2, sigma=2., min_frac=0.2):
         """
         Parameters:
         radius   --   half window size for filtering
         sigma    --   define the range of including pixels (see Sigma filter)
         min_frac --   minimum pixel fraction, average if lower than this fraction
-        pool     --   number of pools for parallel computing
         """
 
         self.radius = radius
         self.sigma = sigma
         self.min_frac = min_frac
         self.min_num = np.floor(self.min_frac * (2*radius + 1)**2)
-        self.pool = pool
 
-    def filter_block(self, pos):
+    def filter_block(self, i, j):
         """
         Perform sigma filtering on specific pixel with position [i, j]
         """
 
-        i, j = pos
         patch = self.I[i-self.radius:i+self.radius+1, j-self.radius:j+self.radius+1]
         valid = np.logical_and(patch>self.sigmaBottom[i, j], patch<self.sigmaTop[i, j])
 
@@ -66,6 +62,9 @@ class SigmaFilter(object):
 
 
     def __call__(self, I):
+        """
+        I -- input image, should be [0, 1]
+        """
 
         self.I = I
         self.hei, self.wid = I.shape
@@ -78,19 +77,30 @@ class SigmaFilter(object):
         self.mean_II = boxfilter(I*I, self.radius) / self.N
         self.var_I = self.mean_II - self.mean_I * self.mean_I
 
-        self.sigmaRange = self.simga * np.sqrt(self.var_I)
+        self.sigmaRange = self.sigma * np.sqrt(self.var_I)
         self.sigmaBottom = self.I - self.sigmaRange
         self.sigmaTop = self.I + self.sigmaRange
 
-        # get all combinations of the position
-        h = np.arange(self.radius:self.hei-self.radius)
-        w = np.arange(self.radius:self.wid-self.radius)
-        pos = np.array(np.meshgrid(h,w)).T.reshape(-1,2)
-        pos = pos.tolist()
-
         print "Start filtering ..."
-        # do filtering in parrallel
-        with Pool(self.pool) as p:
-            self.filtered_I = p.map(self.filter_block, pos)
+        self.filtered_I = np.copy(self.I)
+        for i in range(self.radius, self.hei-self.radius):
+            for j in range(self.radius, self.wid-self.radius):
+                self.filtered_I[i, j] = self.filter_block(i, j)
 
         return self.filtered_I
+
+if __name__ == "__main__":
+
+    import PIL
+    from PIL import Image
+
+    img = Image.open('/Users/xitongyang/Downloads/coins.jpg').convert('L')
+    I = np.array(img, dtype=float)
+    I = I / 255.
+
+    sigma_filter = SigmaFilter(sigma=3)
+    result = sigma_filter(I)
+    result *= 255
+
+    result_img = Image.fromarray(result.astype('uint8'))
+    result_img.save('/Users/xitongyang/Downloads/coins_filtered3.jpg')
