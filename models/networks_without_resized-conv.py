c898f337847f6cdaae79ffa4fbc6f27c32246b58
xyang35 <yangxitongbob@gmail.com>
2017-09-01 00:08:11 -0700

    remove checkbox

--- models/networks_without_resized-conv.py
+++ models/networks_without_resized-conv.py
@@ -325,11 +325,9 @@ class ResnetGenerator(nn.Module):
 
         for i in range(n_downsampling):
             mult = 2**(n_downsampling - i)
-#            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
-#                                         kernel_size=3, stride=2,
-#                                         padding=1, output_padding=1, bias=use_bias),
-            model += [nn.Upsample(scale_factor=2, mode='nearest'),
-                      nn.Conv2d(ngf * mult, int(ngf * mult / 2), 3, padding=1, bias=use_bias),
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+                                         kernel_size=3, stride=2,
+                                         padding=1, output_padding=1, bias=use_bias),
                       norm_layer(int(ngf * mult / 2)),
                       nn.ReLU(True)]
         model += [nn.ReflectionPad2d(3)]
